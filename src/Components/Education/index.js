d40f54b754631aef939ff21041bd9c4611f01939
arturrr03 <rawungito@gmail.com>
2024-11-11 13:51:44 +0800

    finishing all pages

--- src/Components/Education/index.js
+++ src/Components/Education/index.js
@@ -3,8 +3,14 @@ const Education = () => {
     <section id="pendidikan" className="cv-section">
       <h2>Pendidikan</h2>
       <div className="cv-education">
-        <h3>Universitas atau Sekolah</h3>
-        <p>Gelar atau Jurusan | Tahun Lulus</p>
+        <h3>SD Advent Langowan</h3>
+        <p> SD |2009-2015</p>
+        <h3>SMP Negeri 1 Langowan</h3>
+        <p>SMP | 2015-2018</p>
+        <h3>SMK Negeri 1 Langowan</h3>
+        <p>SMK | 2018-2021</p>
+        <h3>Universitas Klabat</h3>
+        <p>Informatika | 2022-2025</p>
       </div>
     </section>
   );
--- src/Components/Sidebar/index.js
+++ src/Components/Sidebar/index.js
@@ -8,32 +8,43 @@ const Sidebar = () => {
       <p>Mahasiswa Universitas Klabat</p>
       <div className="cv-contact">
         <h2>Kontak</h2>
-        <p>Email: Rawungito@gmail.com</p>
-        <p>Telepon: +62 896 3100 8966</p>
+        <p>Email : Rawungito@gmail.com</p>
+        <p>Telepon : +62 896 3100 8966</p>
       </div>
 
       <div className="cv-social">
-        <a
-          href="https://www.instagram.com/arturitoimanuel/?igsh=MXAxMTIxdXdta3VncA%3D%3D"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src="images/ig.png" alt="Instagram" />
-        </a>
-        <a
-          href="https://www.facebook.com/vhira.rharawung?mibextid=ZbWKwL"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src="images/facebook.png" alt="Facebook" />
-        </a>
-        <a
-          href="https://www.instagram.com/arturitoimanuel/?igsh=MXAxMTIxdXdta3VncA%3D%3D"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src="images/git.png" alt="Git Hub" />
-        </a>
+        <div className="social-row">
+          <a
+            href="https://www.instagram.com/arturitoimanuel/?igsh=MXAxMTIxdXdta3VncA%3D%3D"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="images/ig.png" alt="Instagram" />
+          </a>
+          <a
+            href="https://www.facebook.com/vhira.rharawung?mibextid=ZbWKwL"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="images/facebook.png" alt="Facebook" />
+          </a>
+        </div>
+        <div className="social-row">
+          <a
+            href="https://github.com/arturrr03"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="images/git.png" alt="Git Hub" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/arturito-imanuel-b4672b2b3?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="images/in.png" alt="LinkedIn" />
+          </a>
+        </div>
       </div>
     </aside>
   );
